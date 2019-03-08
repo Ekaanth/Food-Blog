ddeb4883632d6bf36ff61c13144c80051e39b5fd
Humblefool94 <akanth1994@gmail.com>
2019-03-08 20:48:47 +0530

    Update

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -10,15 +10,20 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  
-  
+  private instaImages = [];
+
   constructor(private instagramService: InstagramService, private router: Router,
      private articleTagsService: ArticleTagsService) { }
 
   ngOnInit() {
-   
+   this.instagramService.getInstaUserDetails().subscribe(data => this.getInstaUserDetails(data));
   }
 
+  getInstaUserDetails(data) {
+    console.log(data);
+    let instaData = JSON.parse(data._body);
+    this.instaImages = instaData.data;
+  }
 
 
 }
